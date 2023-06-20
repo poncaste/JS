1d951b611dde7484aedacdeb79423edc13753265
Ana Paula de Souza <poncaste@gmail.com>
2023-06-19 23:38:19 -0300

    entrega1

--- app copy 4.js
+++
@@ -1,7 +0,0 @@
-for(let i = 1; i <= 3; i+=1){
-    let dato = prompt("Cargar Nombre")
-    //console.log(dato);
-    //console.log(typeof dato);
-
-    console.log(" Turno Nª " + i + " Nombre: " + dato);
-}
--- app.js
+++ app.js
@@ -1,22 +1,48 @@
-// Condiciona Multiple
+//monedas
+let dolar = 492;
+let euro = 272;
+let real = 86;
 
-// switch
 
-const metodoPago = prompt("Cargar metodo de pago");
-
-switch(metodoPago.toLowerCase()) {
-    case "efectivo":
-        console.log("Pagas con efectivo");
-        break;
-    case "cheque": 
-        console.log("Pagas con cheque");
-        break;
-    case "tarjeta":
-        console.log("Pagas con tarjeta");
-        break;
-    case "mp": 
-        console.log("Pagaste con Mercado Pago");
-        break;
-    default:
-        console.log("No seleccionaste un metodo de pago valido");
+function calculo(num1, num2){
+    let numero = num1/num2;
+    numero = Number(numero.toFixed(2));
+    return numero;
 }
+
+function chequeo(num){
+    if (num>0){
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function casaCambio(){
+
+    alert("Bienvenido a la casa de cambio!");
+    
+    let moneda = prompt("Selecciona cual moneda te gustaria comprar entre Dolar, Real o Euro");
+    let valor = Number(prompt("Ingrese el valor que deseas cambiar en pesos"));
+
+    while (chequeo(valor)){
+        valor = Number(prompt("Valor invalido! Favor ingrese el valor que seas cambiar en pesos"));
+    } 
+
+
+    switch(moneda.toLowerCase()) {
+        case "dolar":
+            alert("Usted recibirá: " + (calculo(valor,dolar)) + " dolares");
+            break;
+        case "real": 
+            alert("Usted recibirá: " + (calculo(valor,real)) + " reales");
+            break;
+        case "euro":
+            alert("Usted recibirá: " + (calculo(valor,euro)) + " euros");
+            break;
+        default:
+            alert("No seleccionaste una moneda valida");
+    }
+}
+
+casaCambio();
